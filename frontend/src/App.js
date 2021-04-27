961e541651f1ac4de85d9e71331228e314b6c6b6
Leah King <dancingheart714@yahoo.com>
2021-04-27 09:22:53 -0400

    installed redux and axios; created reducer, action and component folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginFormMain from './LoginForm/LoginFormMain';
-import './EditEquipment/FormApp'
-import './LoginForm/LoginFormMain'
-
-
+import './EditEquipment/FormApp';
+import './LoginForm/LoginFormMain';
+import React from 'react';
 
 function App() {
   return (
