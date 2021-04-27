73a61fb489fd029ce9728a5039008b71fcbff3a0
Ryder <ryderblue2@gmail.com>
2021-04-26 17:38:58 -0700

    Edit Equipment given basic functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import App from './EditEquipment/App';
-
-// import App from './App';
-
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App from './LoginForm/App'
 
 ReactDOM.render(
   <React.StrictMode>
