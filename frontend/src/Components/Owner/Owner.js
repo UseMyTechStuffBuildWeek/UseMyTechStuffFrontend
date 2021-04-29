067abe9881c194dff600a41ab98d3244454e2f05
Joe Witzke <joewitzke@joes-mbp.lan>
2021-04-29 16:39:46 -0600

    syling for owner page

--- frontend/src/Components/Owner/Owner.js
+++ frontend/src/Components/Owner/Owner.js
@@ -4,6 +4,52 @@ import { axiosWithAuth } from '../../axiosWithAuth';
 import { useHistory } from 'react-router';
 import { getEquipment } from '../../Actions/TechStuffActions';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  font-family: 'Poppins', sans-serif;
+  padding: 5%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: gray;
+
+  img{
+    width: 50%;
+    position: center;
+    object-fit: contain;
+    border-radius: 2%;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+    margin-bottom: 1%;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  button {
+    border-radius: 8px;
+    width: 6rem;
+    height: 2rem;
+    cursor: pointer;
+    background-color: #F8F8F8; 
+    font-weight:700;
+
+    &:hover {
+            background-image: linear-gradient(to bottom, #FE4880, #FFCE00);
+        }
+  }
+`
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 60%;
+  margin: 2%;
+`
 
 const Owner = (props) => {
   const [equipmentList, setEquipmentList] = useState([]);
@@ -38,15 +84,17 @@ const Owner = (props) => {
     <div>
       {equipmentList.map((item) => {
         return (
-          <div>
-            <p>{item.name}</p>
-            <img src={item.imgUrl} />
-            <p>{item.description}</p>
-            <button onClick={() => deleteItem(item)}>Delete Item</button>
-            <button onClick={() => push(`/editequipment/${item.id}`)}>
-              Edit Item
-            </button>
-          </div>
+          <StyledDiv>
+            <h2>{item.name}</h2>
+              <img src={item.imgUrl} />
+              <StyledContainer>
+                <h3>{item.description}</h3>
+                <button onClick={() => deleteItem(item)}>Delete Item</button>
+                <button onClick={() => push(`/editequipment/${item.id}`)}>
+                  Edit Item
+                </button>
+            </StyledContainer>
+          </StyledDiv>
         );
       })}
     </div>
